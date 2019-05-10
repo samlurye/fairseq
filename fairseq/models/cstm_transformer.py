65c0b4722b83b969e260016fc1453bb52a490cb1
Sam Lurye <slurye@Sams-MacBook-Pro-3.local>
2019-05-10 16:18:01 -0400

    Added command to run cstm_transformer in cstm_transformer.py

--- fairseq/models/cstm_transformer.py
+++ fairseq/models/cstm_transformer.py
@@ -14,6 +14,22 @@ import copy
 
 from collections import OrderedDict
 
+"""
+First, upload the flores directory on nc6 to google drive. Then cd into that
+directory and run the following command.
+
+!CUDA_VISIBLE_DEVICES=0 fairseq-train data-bin/wiki_ne_en_bpe5000/ --source-lang en \
+--target-lang ne --arch cstm_transformer --task cstm_translation --share-all-embeddings \
+--encoder-layers 5 --decoder-layers 5 --encoder-embed-dim 512 --decoder-embed-dim 512 \
+--encoder-ffn-embed-dim 2048 --decoder-ffn-embed-dim 2048 --encoder-attention-heads 2 \
+--decoder-attention-heads 2  --encoder-normalize-before --decoder-normalize-before --dropout 0.4 \
+--attention-dropout 0.2 --relu-dropout 0.2 --weight-decay 0.0001 --label-smoothing 0.2 \
+--criterion label_smoothed_cross_entropy --optimizer adam --adam-betas '(0.9, 0.98)' --clip-norm 0 \
+--lr-scheduler inverse_sqrt --warmup-update 4000 --warmup-init-lr 1e-7 --lr 1e-3 --min-lr 1e-9 \
+--max-tokens 1000 --update-freq 4 --max-epoch 100 --save-interval 2 --cstm-share-embeddings \
+--cstm-n-retrieved 4 --fp16
+"""
+
 class CSTMTransformerDecoderLayer(TransformerDecoderLayer):
 
 	def __init__(self, args, no_encoder_attn=False):
